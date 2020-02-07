deed39961684b925d04cd505119f6ce82d82d6ea
katehoang1989 <60130336+katehoang1989@users.noreply.github.com>
2020-02-07 00:45:15 -0600

    Update SHiptv.sh

--- SHiptv.sh
+++ SHiptv.sh
@@ -1,48 +1,48 @@
 #!/bin/sh
 
-#echo 欢迎使用本脚本
+#echo Welcome to this script
 ping 127.0.0.1 -c 2 > /dev/null
-echo 本脚本只适用于merlin改版固件，其他固件请勿运行本脚本
+echo This script is only applicable to merlin modified firmware, please do not run this script for other firmware
 ping 127.0.0.1 -c 2 > /dev/null
-echo 本提示保留10秒，如果您不是merlin改版固件请立刻按下Ctrl+C中止本脚本
+echo This prompt is kept for 10 seconds. If you are not merlin to update the firmware, please press Ctrl + C to terminate this script immediately.
 ping 127.0.0.1 -c 10 > /dev/null
-echo 本脚本适用于上海电信需要AB面认证的IPTV，其他地区请按实际情况修改脚本
+echo This script applies to Shanghai Telecom's IPTV that requires AB certification. Please modify the script for other regions as required.
 ping 127.0.0.1 -c 2 > /dev/null
-echo 使用前请确认光猫已经设置桥接
+echo Please make sure the light cat has been set up before use
 ping 127.0.0.1 -c 2 > /dev/null
 #空行
 echo
 
-#脚本提示
-echo 正在增加IPTV开机自启动脚本
-#移动到脚本目录
-cd /jffs/scripts
-#删除之前的脚本
-echo 正在删除旧文件
+#Script prompt
+echo is increasing the IPTV startup self-startup script
+#Move to the script directory
+cd / jffs / scripts
+#Delete the previous script
+echo is deleting old files
 rm -rf wan-start*
-#下载自启动脚本
-echo 正在下载新文件
-wget -q --no-check-certificate https://raw.githubusercontent.com/PatrikYang/SH-4K-IPTV-Merlin/master/wan-start
-#设置权限
+#Download self-starting script
+echo is downloading a new file
+wget -q --no-check-certificate https://raw.githubusercontent.com/katehoang1989/SH-4K-IPTV-Merlin/master/wan-start
+#Setting permissions
 chmod -R 0755 wan-start
-#完成提示
-echo 成功
+#Complete tips
+echo success
 
-#延迟运行
+#Delayed run
 ping 127.0.0.1 -c 2 > /dev/null
-#空行
+# 空 行
 echo
 
-#脚本提示
-echo 正添加IPTV运行脚本
-#移动到脚本目录
+#Script prompt
+echo is adding IPTV run script
+#Move to the script directory
 cd /jffs/configs/dnsmasq.d
-#删除旧dnsmasq配置文件
-echo 正在删除旧文件
+#Delete the old dnsmasq configuration file
+echo is deleting old files
 rm -rf dns.conf
-#下载开机运行脚本
-echo 正在下载新文件
-wget -q --no-check-certificate https://raw.githubusercontent.com/PatrikYang/SH-4K-IPTV-Merlin/master/dns.conf
+#Download boot run script
+echo is downloading a new file
+wget -q --no-check-certificate https://raw.githubusercontent.com/katehoang1989/SH-4K-IPTV-Merlin/master/dns.conf
 #设置权限
 chmod -R 0644 dns.conf
 #完成提示
@@ -58,7 +58,7 @@ cd
 
 #提示成功
 ping 127.0.0.1 -c 2 > /dev/null
-echo 脚本运行完成，如果光猫已经设置完毕，您可以把IPTV接在Lan1-4任意接口
+echo The script is completed. If the light cat has been set up, you can connect the IPTV to any interface of Lan1-4.
 
 #延迟运行
 ping 127.0.0.1 -c 2 > /dev/null
@@ -66,8 +66,9 @@ ping 127.0.0.1 -c 2 > /dev/null
 echo
 
 #运行提示
-echo 正在重启路由器，请待路由器重启成功后，重新启动IPTV
+echo The router is being restarted, please restart the IPTV after the router is successfully restarted
 #延迟运行
 ping 127.0.0.1 -c 2 > /dev/null
-#重启路由器
+
+#Restart the router
 reboot > /dev/null
